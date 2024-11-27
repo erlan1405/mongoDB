9a1d6e9ffcee02012864c4a93444690c7e9e6353
erlan1405 <erlander5551@gmail.com>
2024-11-27 14:59:50 -0500

    change grades routes

--- expressAPI/routes/grades.mjs
+++ expressAPI/routes/grades.mjs
@@ -4,6 +4,16 @@ import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
+// Get a grades stats data
+router.get("/stats", async (req, res) => {
+  let collection = await db.collection("grades");
+  let query = { class_id: Number(req.params.id) };
+  let result = await collection.find(query).toArray();
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
 // Get a single grade entry
 router.get("/:id", async (req, res) => {
   let collection = await db.collection("grades");
